fb29509fd318486b08e269e293ba609dd023d616
Maxime Biloé <maxime.biloe@prestashop.com>
2016-10-11 17:52:54 +0200

    // Version updated to v1.0.4

--- blockreassurance.php
+++ blockreassurance.php
@@ -38,7 +38,7 @@ class Blockreassurance extends Module implements WidgetInterface
     {
         $this->name = 'blockreassurance';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.3';
+        $this->version = '1.0.4';
         $this->author = 'PrestaShop';
 
         $this->bootstrap = true;
