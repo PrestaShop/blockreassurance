12d91bfd9e9b6b9b7e1c775af055b20c02aa5aa7
Mathieu Ferment <mathieu.ferment@prestashop.com>
2019-06-18 17:59:44 +0200

    Update all index.php files

--- views/templates/hook/index.php
+++ views/templates/hook/index.php
@@ -24,7 +24,12 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-require(dirname(__FILE__).'/../../config/config.inc.php');
-require(dirname(__FILE__).'/../../init.php');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
 
-exit();
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
