8e80c881b3b77b6ca3cabc144013de14fb2fb1f4
Mathieu Ferment <mathieu.ferment@prestashop.com>
2019-06-19 11:52:08 +0200

    Improvements and codestyle on JS

--- views/js/front.js
+++ views/js/front.js
@@ -1,72 +1,72 @@
 /**
-* 2007-2019 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-* @author    PrestaShop SA <contact@prestashop.com>
-* @copyright 2007-2019 PrestaShop SA
-* @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-* International Registered Trademark & Property of PrestaShop SA
-*/
+ * 2007-2019 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2019 PrestaShop SA
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
 
-$(window).ready(function() {
-    $(function(){
-        $('.block-reassurance-item img').each(function(){
-            var $img = jQuery(this);
-            var imgID = $img.attr('id');
-            var imgClass = $img.attr('class');
-            var imgURL = $img.attr('src');
+$(window).ready(function () {
+  $(function () {
+    $('.block-reassurance-item img').each(function () {
+      const $img = jQuery(this);
+      const imgID = $img.attr('id');
+      const imgClass = $img.attr('class');
+      const imgURL = $img.attr('src');
 
-            jQuery.get(imgURL, function(data) {
-                // Get the SVG tag, ignore the rest
-                var $svg = jQuery(data).find('svg');
+      jQuery.get(imgURL, function (data) {
+        // Get the SVG tag, ignore the rest
+        let $svg = jQuery(data).find('svg');
 
-                // Add replaced image's ID to the new SVG
-                if(typeof imgID !== 'undefined') {
-                    $svg = $svg.attr('id', imgID);
-                }
-                // Add replaced image's classes to the new SVG
-                if(typeof imgClass !== 'undefined') {
-                    $svg = $svg.attr('class', imgClass+' replaced-svg');
-                }
+        // Add replaced image's ID to the new SVG
+        if (typeof imgID !== 'undefined') {
+          $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if (typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass + ' replaced-svg');
+        }
 
-                $svg = $svg.attr('data-img-url', imgURL);
+        $svg = $svg.attr('data-img-url', imgURL);
 
-                // Remove any invalid XML tags as per http://validator.w3.org
-                $svg = $svg.removeAttr('xmlns:a');
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
 
-                // Check if the viewport is set, else we gonna set it if we can.
-                if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-                }
+        // Check if the viewport is set, else we gonna set it if we can.
+        if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+          $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
 
-                // Replace image with new SVG
-                $img.replaceWith($svg);
+        // Replace image with new SVG
+        $img.replaceWith($svg);
 
-                // set color of svg at load
-                $('.block-reassurance-item svg path').css('fill', psr_icon_color);
+        // set color of svg at load
+        $('.block-reassurance-item svg path').css('fill', psr_icon_color);
 
-                //$svg.attr('height', '35')
-                $('.block-reassurance-item svg').attr('height', '30');
-                $('.block-reassurance-item svg').attr('width', '30')
+        //$svg.attr('height', '35')
+        $('.block-reassurance-item svg').attr('height', '30');
+        $('.block-reassurance-item svg').attr('width', '30');
 
-                $('.svg').show();
+        $('.svg').show();
 
-            }, 'xml');
-        });
+      }, 'xml');
     });
+  });
 });
