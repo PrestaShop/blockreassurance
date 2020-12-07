799db8e34131e8fdcea6f9debd69be39b5baf43c
NeOMakinG <valentin.szczupak@prestashop.com>
2020-12-07 10:36:34 +0100

    Update webpack to 5.10.0

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, './views/dist'),
+    publicPath: path.resolve(__dirname, './views/dist'),
   },
   module: {
     rules: [
