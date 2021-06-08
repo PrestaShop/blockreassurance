0be724fa4ee572d8e664e44ba1380ed3a25e5942
Mickaël Fernandez <50865159+micka-fdz@users.noreply.github.com>
2021-06-08 12:42:07 +0200

    Update AdminBlockListingController.php

--- controllers/admin/AdminBlockListingController.php
+++ controllers/admin/AdminBlockListingController.php
@@ -76,7 +76,7 @@ class AdminBlockListingController extends ModuleAdminController
             $result = true;
             // Remove Custom icon
             if (!empty($blockPSR['custom_icon'])) {
-                $filePath = str_replace(__PS_BASE_URI__, _PS_ROOT_DIR_ . DIRECTORY_SEPARATOR, $blockPSR['custom_icon']);
+                $filePath = _PS_ROOT_DIR_ . $blockPSR['custom_icon'];
                 if (file_exists($filePath)) {
                     $result = unlink($filePath);
                 }
