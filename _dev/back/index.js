551d578742ca7e28557b517c536265f7820bc068
Paolo Cunti @codencode <info@codencode.it>
2026-02-25 14:14:18 +0100

    fix: hide picto without setting src to "undefined"

--- _dev/back/index.js
+++ _dev/back/index.js
@@ -261,7 +261,7 @@ $(window).ready(() => {
   // Tab Content : Edit : Select none
   $(document).on('click', '#reassurance_block .select_none', () => {
     const psrPicto = $('.psr-picto:visible');
-    psrPicto.attr('src', 'undefined').hide();
+    psrPicto.attr('src', '').hide();
 
     // Un-select icon in the popin
     $('#reassurance_block .category_reassurance img.svg').removeClass('selected');
